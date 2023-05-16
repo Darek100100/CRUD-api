377ea9e0b67ffa2024aa4718922b27cec3df8b8f
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-16 11:41:53 +0200

    Added class represent retrieving from the database

--- movie-library/src/main/java/com/example/movielibrary/MovieController.java
+++ movie-library/src/main/java/com/example/movielibrary/MovieController.java
@@ -1,7 +1,9 @@
 package com.example.movielibrary;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MovieController {
 
     @GetMapping("/test")
