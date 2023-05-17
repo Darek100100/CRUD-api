d655c29fca746e3330a966ab1fc46f2cd42af089
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-17 11:28:49 +0200

    Improved name of files of mapping corrected by adding annotation RequestMapping

--- movie-library/src/main/java/com/example/movielibrary/MovieController.java
+++ movie-library/src/main/java/com/example/movielibrary/MovieController.java
@@ -3,20 +3,22 @@ package com.example.movielibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
-    @GetMapping("/movies")
+    @GetMapping("/")
     public List<Movie> getAll() {
         return movieRepository.getAll();
     }
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public Movie GetById(@PathVariable("id") int id){
         return movieRepository.getById(id);
     }
