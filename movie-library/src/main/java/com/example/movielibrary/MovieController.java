04711415c02b627c2ca62be900497219c07da431
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-17 11:48:27 +0200

    Added endpoint to adding movie to db , and created save method.

--- movie-library/src/main/java/com/example/movielibrary/MovieController.java
+++ movie-library/src/main/java/com/example/movielibrary/MovieController.java
@@ -1,10 +1,7 @@
 package com.example.movielibrary;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,12 +11,20 @@ public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
+
     @GetMapping("/")
     public List<Movie> getAll() {
         return movieRepository.getAll();
     }
+
     @GetMapping("/{id}")
-    public Movie GetById(@PathVariable("id") int id){
+    public Movie GetById(@PathVariable("id") int id) {
         return movieRepository.getById(id);
     }
-}
+
+    @PostMapping("/")
+        public int add(@RequestBody List<Movie> movies) {
+            return movieRepository.save(movies);
+
+        }
+    }
--- movie-library/src/main/java/com/example/movielibrary/MovieRepository.java
+++ movie-library/src/main/java/com/example/movielibrary/MovieRepository.java
@@ -4,23 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.awt.event.MouseMotionAdapter;
 import java.util.List;
 
 @Repository
 public class MovieRepository { //class responsible to communcation from database
     @Autowired
     JdbcTemplate jdbcTemplate;
-
-
     public List<Movie> getAll() {  //methode to return all from db
        return jdbcTemplate.query("SELECT id, name, rating FROM movie",
                 BeanPropertyRowMapper.newInstance(Movie.class));
     }
-
     public Movie getById(int id){ //methode to return one movie from db
         return jdbcTemplate.queryForObject("SELECT id, name, rating FROM movie WHERE id = ?",
                 BeanPropertyRowMapper.newInstance(Movie.class), id);
 
+    }
+    public int save(List<Movie> movies) {
+        movies.forEach(movie -> jdbcTemplate.update("INSERT INTO movie(name, rating) VALUES (?,?)",
+                movie.getName(),movie.getRating()
+        ));
+        return 1;
     }
 
 }
