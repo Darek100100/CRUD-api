70f048687b035516d748c185d8f10c683a83c227
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-17 11:08:47 +0200

    Added method getById to return one movie from db

--- movie-library/src/main/java/com/example/movielibrary/MovieRepository.java
+++ movie-library/src/main/java/com/example/movielibrary/MovieRepository.java
@@ -15,9 +15,12 @@ public class MovieRepository { //class responsible to communcation from database
     public List<Movie> getAll() {  //methode to return all from db
        return jdbcTemplate.query("SELECT id, name, rating FROM movie",
                 BeanPropertyRowMapper.newInstance(Movie.class));
-
-
     }
 
+    public Movie getById(int id){ //methode to return one movie from db
+        return jdbcTemplate.queryForObject("SELECT id FROM movie, name ,rating WHERE id = ?",
+                BeanPropertyRowMapper.newInstance(Movie.class), id);
+
+    }
 
 }
